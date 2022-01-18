7f0e72913b2bd91231d0328421de5ecf39ff7303
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-19 00:15:55 +0700

    adding migration data

--- db/migrate/20220118170836_add_description_column_to_items.rb
+++ db/migrate/20220118170836_add_description_column_to_items.rb
@@ -1,4 +1,5 @@
 class AddDescriptionColumnToItems < ActiveRecord::Migration[7.0]
   def change
+    add_column(:items, :description, :string) # table_name :items, column_name: description, type:string
   end
 end
