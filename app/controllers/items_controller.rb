35ea803254716e91605fe3bf3de7d79f61ef252c
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-19 17:04:03 +0700

    RESTful use link_to in index method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
 
   def index # эндпоинт index
     @items = Item.all # Объявляем инстансную переменную в которую поместим всё что есть в таблице Items
-    render body: @items.map {|i| "#{i.name}:#{i.price}"}
+      # render body: @items.map {|i| "#{i.name}:#{i.price}"}
   end
 
   def create
