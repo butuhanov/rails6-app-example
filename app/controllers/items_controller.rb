f9731a73d40b89c5034e71d897dfbdf86638b8b9
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-19 14:31:04 +0700

    adding route to endpoint

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  def index
+  def index # эндпоинт index
     @items = Item.all # Объявляем инстансную переменную в которую поместим всё что есть в таблице Items
     render text: @items.map do |i| # с помощью метода render вернём текст, где
       # перебираем элементы переменной items  с помощью метода map которому мы передадим блок
