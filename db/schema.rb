c744f8e63db2a5025708bb59d2eacd30ec0afffe
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-19 20:54:10 +0700

    Vote functional - add migration to dev and test environment > rails g migration add_votes_count_to_items > rake db:migrate && rake db:migrate RAILS_ENV=test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_033715) do
+ActiveRecord::Schema.define(version: 2022_01_19_135044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_01_19_033715) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.integer "votes_count", default: 0
     t.index ["name"], name: "index_items_on_name"
     t.index ["price"], name: "index_items_on_price"
   end
