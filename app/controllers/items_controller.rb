d94850817b20503faa36b32991b84f4bdef2fe55
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-19 14:29:18 +0700

    adding method (index) to controller items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all # Объявляем инстансную переменную в которую поместим всё что есть в таблице Items
+    render text: @items.map do |i| # с помощью метода render вернём текст, где
+      # перебираем элементы переменной items  с помощью метода map которому мы передадим блок
+      "#{i.name}:#{i.price}"
+    end
+  end
 end
