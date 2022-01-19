4b3ef20217f9396f048655fcea4c585e03527af3
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-19 14:44:15 +0700

    adding view - second way

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,6 @@ class ItemsController < ApplicationController
 
   def index # эндпоинт index
     @items = Item.all # Объявляем инстансную переменную в которую поместим всё что есть в таблице Items
-    # render text: @items.map do |i| # с помощью метода render вернём текст, где
-    #   # перебираем элементы переменной items  с помощью метода map которому мы передадим блок
-    #   "#{i.name}:#{i.price}"
-    # end
+    render body: @items.map {|i| "#{i.name}:#{i.price}"}
   end
 end
