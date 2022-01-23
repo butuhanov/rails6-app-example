b96d804813167ba0d67f18f8ee5b9cd41c6055be
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-24 00:13:09 +0700

    has_many :through - do migration

--- db/migrate/20220123170856_create_positions.rb
+++ db/migrate/20220123170856_create_positions.rb
@@ -1,7 +1,9 @@
 class CreatePositions < ActiveRecord::Migration[7.0]
   def change
     create_table :positions do |t|
-
+      t.integer :cart_id # корзина
+      t.integer :item_id # товар
+      t.integer :quantity # количество едениц
       t.timestamps
     end
   end
