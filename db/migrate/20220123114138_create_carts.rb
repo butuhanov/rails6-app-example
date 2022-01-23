4535676864d38eceed361eaace77fca7e8ce9125
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-23 18:45:55 +0700

    Set up migrations and link cart to user

--- db/migrate/20220123114138_create_carts.rb
+++ db/migrate/20220123114138_create_carts.rb
@@ -1,7 +1,8 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
-
+      # Корзина должна принадлежать конкретному юзеру
+      t.integer :user_id
       t.timestamps
     end
   end
