834e0d742d44d48aa139698ba94d0cd80b3d61bb
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-23 18:54:16 +0700

    Rspec add tests example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  # Ниже просто заглушка для тестов
+  it 'user test' do;  end
 end
