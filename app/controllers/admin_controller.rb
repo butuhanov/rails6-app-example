cab93aa73e068500af78506cc2c7c684bca71111
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-24 13:34:09 +0700

    Layouts. Using admin layout.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,9 @@
 class AdminController < ApplicationController
   before_action :admin?
+  layout 'admin' # общий лэйаут для всех методов
 
   def users_count
     @users_count = User.count
+    # render layout: 'application' # если хотим использовать индивидуальный лэйаут
   end
 end
