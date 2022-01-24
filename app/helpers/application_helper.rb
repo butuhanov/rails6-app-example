6688a436f2e28614f17e9ccecb78b1c414d31c16
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-24 12:34:34 +0700

    Improve helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,15 @@ module ApplicationHelper
     str.html_safe
   end
 
+  def urls_to_content(str)
+    str.gsub!( %r{https://[^\s<]+}) do |url|
+      if url[/(?:png|jpe?g|gif|svg)$/]
+        "<img src='#{url}' width='100'; />"
+      else
+        "<a href='#{url}'>#{url}</a>"
+      end
+    end
+    str.html_safe
+  end
+
 end
