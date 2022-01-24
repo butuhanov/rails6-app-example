7d3bf70ac44bf1b46ffb61daae51760921756a8e
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-24 10:07:49 +0700

    Polymorphic associations. Fix more bugs

--- db/migrate/20220124025430_create_comments.rb
+++ db/migrate/20220124025430_create_comments.rb
@@ -1,14 +1,14 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string body
-      t.integer user_id
+      t.string  :body
+      t.integer :user_id
       # t.integer item_id
       # t.integer blog_post_id # Предположим, что в будущем понадобится также оставлять комменты под постами в блоге
       # Т.о. пользователь может оставить коммент к товару либо к посту в блоге, но тогда в таблице будет много пустых
       # ячеек. Чтобы этого избежать вводятся полиморфные связи.
-      t.integer commentable_id
-      t.string commentable_type # Item or BlogPost
+      t.integer :commentable_id
+      t.string  :commentable_type # Item or BlogPost
       t.timestamps
     end
   end
