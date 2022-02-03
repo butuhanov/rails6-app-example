bdf2bca05cd139a9854bd5b472f278770966fabc
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-03 18:16:55 +0700

    Testing. Adding controller tests - POST and DELETE methods

--- spec/controllers/item_controller_spec.rb
+++ spec/controllers/item_controller_spec.rb
@@ -3,18 +3,71 @@ require 'rails_helper'
 RSpec.describe ItemsController, type: :controller do
   render_views
 
-  describe 'GET #show' do
-    let(:items) {create_list :item,5}
+  let(:items) {create_list :item,5}
+  let(:item) {create :item}
 
+  let(:items_params) do
+    { item: {
+      name: 'car',
+      price: 50,
+      description: 'black'
+    } }
+  end
+
+  context 'GET #index' do
     before {get :index}
 
     it 'returns items' do
+      is_expected.to render_template :index
       expect(assigns(:items)).to match_array items
-  end
+    end
+
   end
 
-  describe 'DELETE #destroy' do
+  context 'POST #create' do
+    subject {post :create, params: items_params}
 
+    it 'save item' do
+      expect{subject}.to change(Item, :count).by 1
+    end
+
+    it 'redirect to index' do
+      expect(subject).to redirect_to action: :index
+    end
+
+
+    context 'POST #create invalid params' do
+      let(:items_params) do
+        { item: {
+          name: 'car',
+          price: -50,
+          description: 'black'
+        } }
+      end
+
+    it 'POST not saved' do
+      expect{subject}.to_not change(Item, :count)
+    end
+
+      it 'render new template' do
+        is_expected.to render_template :new
+      end
+
+    end
+
+  end
+
+  context 'DELETE #destroy' do
+    subject{delete :destroy, params: params}
+    let (:params) {{id: item.id}}
+
+    it 'delete item' do
+      item.reload
+      expect{subject}.to change(Item, :count).by (-1)
+    end
+    it 'render index template' do
+    is_expected.to redirect_to action: :index
+    end
   end
 
 end
