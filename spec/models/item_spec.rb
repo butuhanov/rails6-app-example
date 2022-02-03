eaad86f4c8b6f03c546f1f85c588520045249de8
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-03 12:21:32 +0700

    RSpec. Check example > bundle exec rspec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-
+  it 'validates price' do
+    # проверяем класс Item
+    # validates :name, :description, presence: true
+    # должно быть presence: true на name
+    should validate_presence_of :name
+  end
 end
