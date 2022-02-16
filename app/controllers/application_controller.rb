0c2edd69d5224d96512f23fea44b2abcfef1318d
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-16 16:18:18 +0700

    users registration from scratch - add views and final edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,16 @@ class ApplicationController < ActionController::Base
   end
 
 
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  helper_method :current_user, :user_signed_in?
+
 end
