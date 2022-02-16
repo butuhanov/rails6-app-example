161a2c68a52a6b456d3ea904aa5e83639215cc53
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-16 15:20:29 +0700

    users registration from scratch - extend users datatable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_10_110500) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_16_080513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,6 +98,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_10_110500) do
     t.string "login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
