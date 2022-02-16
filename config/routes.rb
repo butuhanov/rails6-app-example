9756f531f41d30aac34c7c0d5e08747fb70ccc36
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-16 17:44:58 +0700

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,9 @@ Rails.application.routes.draw do
   end
 
   # для управления пользователями
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
+
+  resource :session, only: %i[new create destroy]
 
   root 'pages#index'
 end
